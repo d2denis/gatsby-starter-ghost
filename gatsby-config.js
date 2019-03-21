0364e5ab9b4903234496d1cccb7d370266a57572
d2denis <zhjpan@gmail.com>
2019-03-21 16:44:43 +0800

    add sitemap
    enable SEO by adding sitemap.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,6 +116,7 @@ module.exports = {
                 ],
             },
         },
+        `gatsby-plugin-sitemap`,
         {
             resolve: `gatsby-plugin-advanced-sitemap`,
             options: {
